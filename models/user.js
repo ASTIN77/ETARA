521fd828ac89724e1e4add0c83b27a7215402444
Andrew <support@astech-it.co.uk>
2018-06-15 07:22:16 +0000

    V1.1.2 - Updated User Model to remove uniqu email requirements

--- models/user.js
+++ models/user.js
@@ -4,8 +4,8 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
         firstName: String,
         lastName: String,
-        username: String,
-        email: {type: String, unique: true, required: true},
+        username: {type: String, unique: true, required: true},
+        email: {type: String, required: true},
         password: String,
         isAdmin: {type: Boolean, default: false},
         isManager: {type: Boolean, default: false}
