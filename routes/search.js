04555c78bdd1edbbe1c9435574331e850b5e2a0d
Andrew Tinsley <andrew@Andrews-Mac-mini.local>
2021-06-30 17:38:39 +0100

    Added console logging for debugging purposes on search results

--- routes/search.js
+++ routes/search.js
@@ -1,19 +1,19 @@
-const   express             =       require("express"),
-        router              =       express.Router({mergeParams: true}),
-        Fault               =       require("../models/fault"),
-        Mprn                =       require("../models/mprn"),
-        middleware          =       require("../middleware/middleware");
-        
+const express = require("express"),
+    router = express.Router({ mergeParams: true }),
+    Fault = require("../models/fault"),
+    Mprn = require("../models/mprn"),
+    middleware = require("../middleware/middleware");
 
-router.post("/", middleware.isLoggedIn, (req,res) => {
+
+router.post("/", middleware.isLoggedIn, (req, res) => {
 
     let ticket = req.body.ticketRef, mprn = req.body.mprnRef;
-    let ticketQuery = {'jobRef': ticket}, mprnQuery = {'mprNo': mprn};
+    let ticketQuery = { 'jobRef': ticket }, mprnQuery = { 'mprNo': mprn };
 
     if (ticket.length) { // If ticket input field is not empty/undefined
 
-        Fault.findOne(ticketQuery).populate("comments").exec(function(err, foundFault){
-            if(err){
+        Fault.findOne(ticketQuery).populate("comments").exec(function (err, foundFault) {
+            if (err) {
                 req.flash("error", "Please provide a valid Ticket Reference!");
                 res.redirect("/");
             } else {
@@ -27,34 +27,34 @@ router.post("/", middleware.isLoggedIn, (req,res) => {
         });
     }
     if (mprn) { // If mprn number input box is not empty/undefined
-        
+
         Mprn.find(mprnQuery).populate("comments").exec((err, foundMprn) => {
-            if(err){
+            if (err) {
                 req.flash("confirm", "Please provide a valid MPRN!");
                 res.redirect("/");
             } else {
-            if (!foundMprn.length){
-                req.flash("confirm", "No Details found for this Meter Point.");
-                res.redirect("/");
+                if (!foundMprn.length) {
+                    req.flash("confirm", "No Details found for this Meter Point.");
+                    res.redirect("/");
                 }
 
-            Fault.find(mprnQuery).populate("comments").exec((err,foundFault) => {
-                if(err){
-                    req.flash("error", "Something went wrong. Please try again.")
-                }
-                if(!foundFault.length){
-                    foundFault = "-";
-                }
-            })
+                Fault.find(mprnQuery).populate("comments").exec((err, foundFault) => {
+                    if (err) {
+                        req.flash("error", "Something went wrong. Please try again.")
+                    }
+                    if (!foundFault.length) {
+                        foundFault = "-";
+                    }
+                })
+                console.log(faults);
+                res.render("search/results", { faults: foundMprn });
 
-            res.render("search/results", {faults: foundMprn});
-                    
-                
+
             }
         });
-    } 
-    
-    if(!ticket.length && !mprn){ // if mpnr and ticket ref are both blank
+    }
+
+    if (!ticket.length && !mprn) { // if mpnr and ticket ref are both blank
         req.flash("error", "Please provide a valid Ticket Reference or valid Meter Point Reference Number.");
         res.redirect("/");
     }
@@ -63,19 +63,19 @@ router.post("/", middleware.isLoggedIn, (req,res) => {
 // SHOW FAULT TICKET ROUTE
 
 router.get("/:id", middleware.isLoggedIn, (req, res) => {
-    
+
     Fault.findById(req.params.id).populate("comments").exec((err, foundFault) => {
-    if (err) {
-        console.log(err);
-    } else {
-        var query = {'mprNo': foundFault.mprNo};
-        Mprn.findOne(query, (err, foundMprn) => {
-            if (err) {
-                req.flash("error", "Please neter a valid mprn!");
-                res.redirect("back");
+        if (err) {
+            console.log(err);
+        } else {
+            var query = { 'mprNo': foundFault.mprNo };
+            Mprn.findOne(query, (err, foundMprn) => {
+                if (err) {
+                    req.flash("error", "Please neter a valid mprn!");
+                    res.redirect("back");
                 } else {
-                    res.render("search/show", {fault: foundFault, mprn: foundMprn} );
-                    }
+                    res.render("search/show", { fault: foundFault, mprn: foundMprn });
+                }
             });
         }
     });
