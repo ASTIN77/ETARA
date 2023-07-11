565007ae8246cb68c21e43eb951279d93b706c04
Andrew Tinsley <andrew@astinsley.net>
2023-07-11 20:36:16 +0100

    Updated req.logout function

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,25 +1,29 @@
 var middlewareObj = {};
 
-
-middlewareObj.checkCurrentUser = function(req,res,next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    return res.redirect("/login");
+middlewareObj.checkCurrentUser = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  return res.redirect("/login");
 };
 
-middlewareObj.isLoggedIn = (req,res,next) => {
-    if(req.isAuthenticated()){
-        return next();
-    }
-    req.flash("error", "You need to be logged in to do that!");
-    return res.redirect("/login");
+middlewareObj.isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  req.flash("error", "You need to be logged in to do that!");
+  return res.redirect("/login");
 };
 
 middlewareObj.logout = (req, res, next) => {
-        req.logout();
-        req.flash("success", "You are now logged out.");
-        return next();
-    };
-    
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "You are now logged out.");
+    return next();
+    //res.redirect('/');
+  });
+};
+
 module.exports = middlewareObj;
